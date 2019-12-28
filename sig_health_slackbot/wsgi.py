9ceb4c1ccef969d3c507881e98306cbdef6d6803
Chanwook Kim <chanwook@toss.im>
2019-12-28 20:27:08 +0900

    Pre commit 설정, run all files

--- sig_health_slackbot/wsgi.py
+++ sig_health_slackbot/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sig_health_slackbot.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sig_health_slackbot.settings")
 
 application = get_wsgi_application()
